f67a21c140b86549761fcde7453a6bcd96bd991f
UkraGames <diegobarbosa090305@gmail.com>
2025-02-18 06:52:47 -0500

    Cambios menores

--- src/main/java/jar/gymhub/Main.java
+++ src/main/java/jar/gymhub/Main.java
@@ -8,7 +8,7 @@ package jar.gymhub;
  *
  * @author diego
  */
-public class GymHub {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
