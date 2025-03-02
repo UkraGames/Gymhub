fa6e2a4153a2ab65044ff63a3eb74bef7405642f
Alejosamy96 <Alejo@192.168.39.127>
2025-03-01 21:05:25 -0500

    prueba actualización

--- src/main/java/jar/gymhub/Main.java
+++ src/main/java/jar/gymhub/Main.java
@@ -1,18 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package jar.gymhub;
-
-/**
- *
- * @author diego
- */
 public class Main {
-
     public static void main(String[] args) {
-        //Aquí va una nueva línea
-        System.out.print("Todos son geis");
-        
+        System.out.print("hola");
     }
 }
