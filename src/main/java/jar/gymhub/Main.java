94e255a4d96b15e7c2cfc3f33e83c0447f2ea20a
UkraGames <diegobarbosa090305@gmail.com>
2025-03-14 18:44:43 -0500

    Creation of a dataBase Creator

--- src/main/java/jar/gymhub/Main.java
+++ src/main/java/jar/gymhub/Main.java
@@ -1,13 +1,19 @@
 package jar.gymhub;
-import jar.gymhub.db.gymConnect;
+
+import jar.gymhub.classes.client;
+import jar.gymhub.dao.daoClientImpl;
+import jar.gymhub.db.dataBase;
+import jar.gymhub.faces.daoClient;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
         //Aquí va una nueva línea
-       gymConnect con = new gymConnect("/home/Descargas/dataBase.db");
-       
-       con.getConnection();
-       //holamundo
-    }
+        new dataBase();
+        
+        }
 }
+
