857fe6ce2a4d380e924228ab858221b031265a28
Julian Andres Salamanca Barreto <julians2051@hotmail.com>
2025-03-28 19:31:22 -0500

    GUI usuarios 2.1

--- src/main/java/jar/gymhub/Main.java
+++ src/main/java/jar/gymhub/Main.java
@@ -1,17 +1,10 @@
 package jar.gymhub;
-import jar.gymhub.db.gymConnect;
+
+
 
 public class Main {
-
     public static void main(String[] args) {
-        //Aquí va una nueva línea
-       gymConnect con = new gymConnect("C:\\Users\\DELL\\Downloads\\VM\\Nueva carpeta\\hola.db");
-       
-       var system = System.getProperty("os.name");
-       System.out.println(system);
-       
-       
-       con.getConnection();
-       //holamundo
+        String s = System.getProperty("user.home");
+        System.out.println("El directorio de usuario es: " + s);
     }
 }
