2c3a506b267177681cc560d7738f7a3d617ad6db
UkraGames <diegobarbosa090305@gmail.com>
2025-05-18 14:44:18 -0500

    Changed name of Services package to service package

--- src/main/java/com/gymhub/services/AdminService.java
+++ src/main/java/com/gymhub/services/AdminService.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.gymhub.Services;
+package com.gymhub.services;
 
 import com.gymhub.dao.DaoUserAcces;
 import com.gymhub.model.UserAcces;
--- src/main/java/com/gymhub/services/StateService.java
+++ src/main/java/com/gymhub/services/StateService.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.gymhub.Services;
+package com.gymhub.services;
 
 import com.gymhub.dao.DaoStateImpl;
 import com.gymhub.model.State;
