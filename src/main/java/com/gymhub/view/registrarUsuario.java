7c7ef06dbb2e68e6c86abe1c80a1d5181259ba83
Alejosamy96 <Alejo@192.168.39.127>
2025-05-20 08:40:22 -0500

    Merge branch 'backend' into GUI
    # Conflicts:
    #	src/main/java/com/gymhub/db/GymConnect.java
    #	src/main/java/jar/gymhub/Main.java

--- src/main/java/com/gymhub/view/registrarUsuario.java
+++ src/main/java/com/gymhub/view/registrarUsuario.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 package com.gymhub.view;
-
 /**
  *
      * @author Alejo
@@ -280,7 +279,7 @@ public class registrarUsuario extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void NombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NombreActionPerformed
